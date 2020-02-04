d496d74a33f2f2e63812c2e7a57733b446d0b0dc
Kathy Tran <44452243+katran009@users.noreply.github.com>
2020-02-04 14:46:06 -0600

    updating string1 & string2.py

--- string2.py
+++ string2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python -tt
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -15,11 +15,16 @@
 # add 'ly' instead.
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
-
-
 def verbing(s):
-    """Your code goes here.  Edit this docstring."""
-    return
+  length = len(s)
+
+  if length > 2:
+    if s[-3:] == 'ing':
+      s += 'ly'
+    else:
+      s += 'ing'
+
+  return s
 
 
 # E. not_bad
@@ -31,8 +36,13 @@ def verbing(s):
 # So 'This dinner is not that bad!' yields:
 # This dinner is good!
 def not_bad(s):
-    """Your code goes here.  Edit this docstring."""
-    return
+  snot = s.find('not')
+  sbad = s.find('bad')
+
+  if sbad > snot:
+    s = s.replace(s[snot:(sbad+3)], 'good')
+
+  return s
 
 
 # F. front_back
@@ -43,44 +53,58 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
-    """Your code goes here.  Edit this docstring."""
-    return
+  alength = len(a)
+  blength = len(b)
+
+  if alength % 2 == 0:
+    aindex = alength // 2
+  else:
+    aindex = (alength // 2) + 1
+
+  if blength % 2 == 0:
+    bindex = blength // 2
+  else:
+    bindex = (blength // 2) + 1
+
+  afront = a[0:aindex]
+  aback = a[aindex:]
+
+  bfront = b[0:bindex]
+  bback = b[bindex:]
+
+  return afront + bfront + aback + bback
 
 
-# Provided simple test() function used in main() to print
+# Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
-    """Your code goes here.  Edit this docstring."""
-    if got == expected:
-        prefix = ' OK '
-    else:
-        prefix = '  X '
-    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
 
 
 # main() calls the above functions with interesting inputs,
 # using the above test() to check if the result is correct or not.
 def main():
-    """Your code goes here.  Edit this docstring."""
-    print('verbing')
-    test(verbing('hail'), 'hailing')
-    test(verbing('swiming'), 'swimingly')
-    test(verbing('do'), 'do')
+  print 'verbing'
+  test(verbing('hail'), 'hailing')
+  test(verbing('swiming'), 'swimingly')
+  test(verbing('do'), 'do')
 
-    print()
-    print('not_bad')
-    test(not_bad('This movie is not so bad'), 'This movie is good')
-    test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
-    test(not_bad('This tea is not hot'), 'This tea is not hot')
-    test(not_bad("It's bad yet not"), "It's bad yet not")
+  print
+  print 'not_bad'
+  test(not_bad('This movie is not so bad'), 'This movie is good')
+  test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+  test(not_bad('This tea is not hot'), 'This tea is not hot')
+  test(not_bad("It's bad yet not"), "It's bad yet not")
 
-    print()
-    print('front_back')
-    test(front_back('abcd', 'xy'), 'abxcdy')
-    test(front_back('abcde', 'xyz'), 'abcxydez')
-    test(front_back('Kitten', 'Donut'), 'KitDontenut')
+  print
+  print 'front_back'
+  test(front_back('abcd', 'xy'), 'abxcdy')
+  test(front_back('abcde', 'xyz'), 'abcxydez')
+  test(front_back('Kitten', 'Donut'), 'KitDontenut')
 
-
-# Standard boilerplate (python idiom) to call the main() function.
 if __name__ == '__main__':
-    main()
+  main()
